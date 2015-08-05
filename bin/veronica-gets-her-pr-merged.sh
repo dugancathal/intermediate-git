cfaa53f087ae0f76030aea14d8c9af936088f1fc
TJ Taylor <dugancathal@gmail.com>
2015-08-05 11:15:18 -0600

    Add some notes and a push after merge resolution

--- bin/veronica-gets-her-pr-merged.sh
+++ bin/veronica-gets-her-pr-merged.sh
@@ -4,9 +4,18 @@ source $curdir/helpers.sh
 source $curdir/veronica-config.sh
 
 inpane 0
+echo "Tim: Looks over Veronica's changes"
 telltmux "git remote add veronica $repo"
 telltmux "git fetch veronica"
 telltmux "git checkout veronica/pings-have-creation-times"
 telltmux "git show"
 
+echo 'Explain detached head'
+echo 'Explain that git has GREAT help messages'
+echo 'Explain the issue we created - one line caused problems'
+echo ''
+echo 'Resolve the merge conflict'
+
 wait_for_input
+
+telltmux 'git push -u origin master'
